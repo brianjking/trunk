411f109fbc51c6503e95c8a96eacc8a2febc3bd7
Tim Davies <mail@timdavies.io>
2015-11-01 16:36:48 +0000

    Require authentication for notes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "homepage#index"
   devise_for :users
 
-  resources :notes do
-    get 'search', on: :collection
+  authenticate :user do
+    resources :notes do
+      get 'search', on: :collection
+    end
   end
 end
