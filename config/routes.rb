a616fd2cd62f8190fe9cf29e0a0ccaca6fd3b5ab
Tim Davies <mail@timdavies.io>
2015-12-17 19:34:32 +0000

    Hook up archive link

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :notes do
       get 'search', on: :collection
       get 'archive', on: :collection
-      post 'restore/:id', on: :collection
+      post 'restore/:id', to: 'notes#restore', on: :collection, as: :restore
     end
   end
 end
