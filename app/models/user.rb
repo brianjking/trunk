c1de7a1b6d5bd9eec500a5e8a21100af52f5f803
Tim Davies <mail@timdavies.io>
2015-12-20 01:03:03 +0000

    Add dependent destroy to user note relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :notes
+  has_many :notes, dependent: :destroy
 end
